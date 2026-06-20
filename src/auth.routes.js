5097d566a551c323c964cebbd0553d222b6d8f32
Student ApnaCollege <chaubeynitish547@gmail.com>
2026-06-20 17:10:33 +0530

    add all the files

---
+++ src/auth.routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const authController = require("./controllers/auth.controllers");
+
+const authRouter = express.Router();
+
+authRouter.post(
+    "/register",
+    authController.registerUserController
+);
+authRouter.post("/login",authController.loginUserController)
+authRouter.get("/logout",authController.logoutUserController)
+
+module.exports = authRouter;
